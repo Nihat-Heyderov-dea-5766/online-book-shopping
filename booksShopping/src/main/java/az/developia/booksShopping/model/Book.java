e47bc9c6e1e13bdc8b52fbd5cc3253c7e2e248a2
Nihat-Heyderov-dea-5766 <86143636+Nihat-Heyderov-dea-5766@users.noreply.github.com>
2021-10-01 18:30:33 +0400

    mysql e descriptionu elave etmir

--- booksShopping/src/main/java/az/developia/booksShopping/model/Book.java
+++ booksShopping/src/main/java/az/developia/booksShopping/model/Book.java
@@ -12,6 +12,14 @@ public class Book {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	private String description;
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	public Integer getId() {
 		return id;
 	}
