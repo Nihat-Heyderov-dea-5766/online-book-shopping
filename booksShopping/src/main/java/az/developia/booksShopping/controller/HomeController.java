10b8626906dec5a88030ea635c3edfbf9b4b83ad
Nihat-Heyderov-dea-5766 <86143636+Nihat-Heyderov-dea-5766@users.noreply.github.com>
2021-11-19 18:36:59 +0400

    lahiye bitdi amma problemler var

--- booksShopping/src/main/java/az/developia/booksShopping/controller/HomeController.java
+++ booksShopping/src/main/java/az/developia/booksShopping/controller/HomeController.java
@@ -2,9 +2,11 @@ package az.developia.booksShopping.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import az.developia.booksShopping.config.MySession;
+
 
 
 
@@ -15,7 +17,15 @@ public class HomeController {
 	private MySession mySession;
 	
 @GetMapping(path={"/home","/"})
-public String showHomePage() {
+public String showHomePage(Model model) {
+System.out.println(mySession.getBasketBooks());
+
+String username = mySession.getUsername();
+if(username.equals("anonymousUser")) {
+	
+}else {
+	model.addAttribute("username",username);
+}
 
 	return "home";
 }
