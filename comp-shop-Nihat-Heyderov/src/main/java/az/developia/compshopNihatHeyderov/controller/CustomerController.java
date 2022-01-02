50f8a2a8d418dd584080b2554986c5747933ccd0
Nihat-Heyderov-dea-5766 <86143636+Nihat-Heyderov-dea-5766@users.noreply.github.com>
2022-01-03 03:28:33 +0400

    details bolmesi

--- comp-shop-Nihat-Heyderov/src/main/java/az/developia/compshopNihatHeyderov/controller/CustomerController.java
+++ comp-shop-Nihat-Heyderov/src/main/java/az/developia/compshopNihatHeyderov/controller/CustomerController.java
@@ -3,11 +3,13 @@ package az.developia.compshopNihatHeyderov.controller;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import az.developia.compshopNihatHeyderov.config.MySession;
 import az.developia.compshopNihatHeyderov.dao.ComputerDAO;
@@ -27,5 +29,17 @@ public class CustomerController {
 		List<Computer> computers = computerDAO.findAllByUsername(mySession.getUsername());
 		model.addAttribute("computers",computers);
 		return "customer";
+	}
+	
+	@GetMapping(path="/computers/details/{id}")
+	public String showDetailsPage(@ModelAttribute(name="id")Integer id,Model model) {
+  Optional<Computer> computersOptional= computerDAO.findById(id);
+  boolean compExits = computersOptional.isPresent();
+  Computer computers = new Computer();
+  if(compExits) {
+	  computers = computersOptional.get();
+  }
+	model.addAttribute("computers",computers);
+		return "computer-details";
 	}
 }
